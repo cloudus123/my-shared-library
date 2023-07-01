71699560e372514aa8792e56ee929ffae5da760d
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-01 22:09:30 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,11 +1,12 @@
-def call(String name, String name2, String containername, String port = "") {
+def call(String name, String name2, String containername, String port = "", String port = "", String mountvolume "") {
     script {
         def dockerRunCommand = "docker run -d --name ${containername}"
         
         if (port) {
             dockerRunCommand += " -p ${port}:${port}"
         }
-        
+        if (mountvolume) {
+            dockerRunCommand += "-v ${mountvolume}"
         dockerRunCommand += " ${name}"
         
         sh """
