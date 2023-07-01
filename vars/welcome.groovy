c210a467365ce6f0ac9220f5a04caed0de47dd59
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-01 20:09:23 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -2,6 +2,8 @@ def call (String name, String name2) {
     script {
          sh """
             docker build -t ${name} -f ${name2} .
+            touch new
+            ls -lrth >>new
             """
     }
 }
