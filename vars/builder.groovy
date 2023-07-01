b1d114f577b482d4e7aa0a0f52bf02c108242737
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-01 18:24:18 +0530

    Update builder.groovy

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -1,23 +1,13 @@
-package org.jenkinsci.plugins.docker.workflow
-
-import org.jenkinsci.plugins.workflow.cps.CpsScript
-
-class DockerBuilder implements Serializable {
-    CpsScript script
-    Docker docker
-
-    DockerBuilder(CpsScript script) {
-        this.script = script
-        this.docker = new Docker(script)
+def image_build_and_run(String imageName, String containerName, String dockerfilePath, String contextPath, String port = '', String containerPort = '') {
+    stage("Build Docker Image") {
+		steps {
+        sh "docker build -t ${imageName} -f ${dockerfilePath} ${contextPath}"
+		}
     }
-
-    def buildAndRun(String imageName, String containerName, String dockerfilePath, String contextPath, String port = '', String containerPort = '') {
-        script.stage("Build Docker Image") {
-            script.sh "docker build -t ${imageName} -f ${dockerfilePath} ${contextPath}"
-        }
-        script.stage("Run container image") {
-            def publishOption = port ? "-p ${port}:${containerPort}" : ""
-            script.sh "docker run -d ${publishOption} --name ${containerName} ${imageName}"
-        }
+    stage("Run container image") {
+		steps {
+        def publishOption = port ? "-p ${port}:${containerPort}" : ""
+        sh "docker run -d ${publishOption} --name ${containerName} ${imageName}"
+		}
     }
 }
