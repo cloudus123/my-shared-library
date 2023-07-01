1891c400b2d0cc29ef78702aac7b175e5ca6769b
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-01 19:25:10 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,7 +1,5 @@
 def call (String name, String name2) {
-    stage ("build") {
-        steps {
-            echo "HI, ${name} and ${name2}"
-        }
+    script {
+        echo "HI, ${name} and ${name2}"
     }
 }
