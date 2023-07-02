65adac36629751aa0af6d6046639714469a51547
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-02 12:53:12 +0530

    Update name.groovy

--- vars/name.groovy
+++ vars/name.groovy
@@ -1,15 +1,15 @@
 def call(String name, String pathtodocker, String contextPath, String containername, String port = "", String port1 = "") {
     script {
-        
-        sh """
-            docker build -t ${name} -f ${pathtodocker} ${contextPath}
-            ${dockerRunCommand}
-        """
         def dockerRunCommand = "docker run -d --name ${containername}"
         
         if (port && port1) {
             dockerRunCommand += " -p ${port}:${port1}"
         }
         dockerRunCommand += " ${name}"
+        sh """
+            docker build -t ${name} -f ${pathtodocker} ${contextPath}
+            ${dockerRunCommand}
+        """
+        
     }
 }
