9069aaa4eba47a27c31503e8944ec3c028936481
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-16 12:47:04 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,8 +1,9 @@
-def call(String name, String pathtodocker, String contextPath) {
+def call(String name, String pathtodocker, String contextPath, String ECRurl) {
     script {
         sh """
             docker build -t ${name} -f ${pathtodocker} ${contextPath}
-            
+            docker tag ${nmae} ${ECRurl}/${name}
+            docker push ${ECRurl}/${name}
         """        
     }
 }
